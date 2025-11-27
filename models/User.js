952d615d169b9b7112565ab1af854b50d0515390
xurshid686 <aliqulovxurshid24@gmail.com>
2025-11-27 17:53:40 +0500

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['superadmin', 'teacher', 'student'], required: true },
+  isActive: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
